f7ee1e5d421bb258225cb8ced631f1cfe8cf2a06
SajeedAninda <sajaninda@gmail.com>
2024-09-02 17:18:45 +0600

    Created functionality for dark mode

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import logo from "../assets/logo.svg"
-import moonLogo from "../assets/Moon_fill.svg"
+import React, { useEffect, useState } from 'react';
+import logo from "../assets/logo.svg";
+import moonLogo from "../assets/Moon_fill.svg";
+import sunLogo from "../assets/Sun_fill.svg";
 
 const Navbar = () => {
-    return (
-        <div className='bg-[#F9FAFBCC] h-[13vh] flex items-center border-b border-[#E5E7EB]'>
-            <div className='w-[90%] mx-auto flex justify-between items-center'>
-                <img className='w-[180px]' src={logo} alt="" />
+    const [darkMode, setDarkMode] = useState(false);
 
-                <div className='p-2 border border-[#E5E7EB] cursor-pointer hover:border-[#4D5562] rounded-xl'>
-                    <img src={moonLogo} alt="" />
+    useEffect(() => {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme === 'dark') {
+            document.documentElement.classList.add('dark');
+            setDarkMode(true);
+        }
+    }, []);
+
+    const toggleDarkMode = () => {
+        const newDarkMode = !darkMode;
+        setDarkMode(newDarkMode);
+
+        if (newDarkMode) {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    };
+
+    return (
+        <div className='bg-[#F9FAFBCC] dark:bg-gray-900 h-[13vh] flex items-center border-b border-[#E5E7EB] dark:border-gray-700'>
+            <div className='w-[90%] mx-auto flex justify-between items-center'>
+                <img className='w-[180px]' src={logo} alt="Logo" />
+
+                <div
+                    onClick={toggleDarkMode}
+                    className='p-2 border border-[#E5E7EB] dark:border-gray-700 cursor-pointer hover:border-[#4D5562] dark:hover:border-gray-500 rounded-xl'
+                >
+                    <img src={darkMode ? sunLogo : moonLogo} alt={darkMode ? "Sun Logo" : "Moon Logo"} />
                 </div>
             </div>
         </div>
